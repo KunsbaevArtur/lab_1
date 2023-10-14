14434871ce7d3b3e2f73b3a86c02287d9a19f378
KunsbaevArtur <147943133+KunsbaevArtur@users.noreply.github.com>
2023-10-14 23:16:27 +0500

    Add files via upload

---
+++ 1 лабораторная/2_задание/main_2.py
@@ -0,0 +1,7 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+total = len(list_players)
+first = list_players[:3]
+second = list_players[3:]
+print(first)
+print(second)
+# TODO Разделите участников на две команды
